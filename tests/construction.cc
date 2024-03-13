14d068ad6ea3ca1542077822694b7a573337b1b6
Donald Lee <donaldturinglee@gmail.com>
2024-03-13 02:40:33 -0400

    test: Add test for logging complicated message

--- tests/construction.cc
+++ tests/construction.cc
@@ -6,9 +6,19 @@ TEST("Simple message can be logged") {
 	std::string message = "simple ";
 	message += Utility::random_string();
 
-	cclog::log() << message << " with more text.\n";
+	cclog::log() << message << " with more text.";
 
 	bool result = Utility::is_text_in_file(message, "cclog.log");
 
+	CONFIRM_TRUE(result);
+}
+
+TEST("Complicated message can be logged") {
+	std::string message = "complicated ";
+	message += Utility::random_string();
+	cclog::log() << message
+				<< " double=" << 3.14
+				<< " quoted=" << std::quoted("in quotes");
+	bool result = Utility::is_text_in_file(message, "cclog.log");
 	CONFIRM_TRUE(result);
 }
