cc5595c78589f1a0f7dfeb6e22fedd4acda78dc1
Donald Lee <donaldturinglee@gmail.com>
2024-03-14 01:16:27 -0400

    refactor: the tag design with TDD

--- include/cclog.h
+++ include/cclog.h
@@ -6,9 +6,10 @@
 #include <ctime>
 #include <fstream>
 #include <iomanip>
-#include <iostream>
 #include <map>
 #include <memory>
+#include <ostream>
+#include <sstream>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -25,22 +26,187 @@ public:
 		return text_;
 	}
 	virtual std::unique_ptr<Tag> clone() const = 0;
+	virtual bool match(Tag const& source) const = 0;
+
 protected:
-	Tag(std::string const& key, std::string const& value) : key_(key), text_(key + "=\"" + value + "\"") {}
-	Tag(std::string const& key, int value) : key_(key), text_(key + "=" + std::to_string(value)) {}
-	Tag(std::string const& key, long long value) : key_(key), text_(key + "=" + std::to_string(value)) {}
-	Tag(std::string const& key, double value) : key_(key), text_(key + "=" + std::to_string(value)) {}
-	Tag(std::string const& key, bool value) : key_(key), text_(key + "=" + (value ? "true" : "false")) {}
+	Tag (std::string const& key, std::string const& value) : key_(key), text_(key + "=\"" + value + "\"") {}
+    Tag (std::string const& key, int value) : key_(key), text_(key + "=" + std::to_string(value)) {}
+    Tag (std::string const& key, long long value) : key_(key), text_(key + "=" + std::to_string(value)) {}
+    Tag (std::string const& key, double value) : key_(key), text_(key + "=" + std::to_string(value)) {}
+    Tag (std::string const & key, bool value) : key_(key), text_(key + "=" + (value?"true":"false")){}
 
 private:
 	std::string key_;
 	std::string const text_;
 };
 
-
 inline std::string to_string(Tag const& tag) {
 	return tag.get_text();
 }
+
+inline std::ostream& operator<<(std::ostream&& os, Tag const& tag) {
+	os << to_string(tag);
+	return os;
+}
+
+enum class TagOperation {
+	kNone,
+	kEqual,
+	kLessThan,
+	kLessThanOrEqual,
+	kGreaterThan,
+	kGreaterThanOrEqual
+};
+
+template<typename T, typename ValueType>
+class TagType : public Tag {
+public:
+	std::unique_ptr<Tag> clone() const override {
+		return std::unique_ptr<Tag>(new T(*static_cast<T const*>(this)));
+	}
+
+	bool match(Tag const& source) const override {
+		if(get_key() != source.get_key()) {
+			return false;
+		}
+		TagType const& source_cast = static_cast<TagType const&>(source);
+		if(operation_ == TagOperation::kNone) {
+			switch(source_cast.operation_) {
+				case TagOperation::kNone:
+					return value_ == source_cast.value_;
+				default:
+					return compare_tag_types(value_, source_cast.operation_, source_cast.value_);
+			}
+		}
+		switch(source_cast.operation_) {
+			case TagOperation::kNone:
+				return compare_tag_types(source_cast.value_, operation_, value_);
+			default:
+				return false;
+		}
+	}
+
+	ValueType get_value() const {
+		return value_;
+	}
+
+protected:
+	TagType(ValueType const& value, TagOperation operation) : Tag(T::key, value), value_(value), operation_(operation) {}
+	virtual bool compare_tag_types(ValueType const& value, TagOperation operation, ValueType const& criteria) const {
+		return false;
+	}
+    ValueType value_;
+    TagOperation operation_;
+};
+
+template<typename T>
+class StringTagType : public TagType<T, std::string> {
+protected:
+	StringTagType(std::string const& value, TagOperation operation) : TagType<T, std::string>(value, operation) {}
+	bool compare_tag_types(std::string const& value, TagOperation operation, std::string const& criteria) const override {
+		int result = value.compare(criteria);
+		switch(operation) {
+			case TagOperation::kEqual:
+				return result == 0;
+			case TagOperation::kLessThan:
+				return result == -1;
+			case TagOperation::kLessThanOrEqual:
+				return result == 0 || result == -1;
+			case TagOperation::kGreaterThan:
+				return result == 1;
+			case TagOperation::kGreaterThanOrEqual:
+				return result == 0 || result == 1;
+			default:
+				return false;
+		}
+	}
+};
+
+template<typename T>
+class IntegerTagType : public TagType<T, int> {
+protected:
+	IntegerTagType(int const& value, TagOperation operation) : TagType<T, int>(value, operation) {}
+	bool compare_tag_types(int const& value, TagOperation operation, int const& criteria) const override {
+		switch(operation) {
+			case TagOperation::kEqual:
+				return value == criteria;
+			case TagOperation::kLessThan:
+				return value < criteria ;
+			case TagOperation::kLessThanOrEqual:
+				return value <= criteria;
+			case TagOperation::kGreaterThan:
+				return value > criteria;
+			case TagOperation::kGreaterThanOrEqual:
+				return value >= criteria;
+			default:
+				return false;
+		}
+	}
+};
+
+template<typename T>
+class LongLongTagType : public TagType<T, long long> {
+protected:
+	LongLongTagType(long long const& value, TagOperation operation) : TagType<T, long long>(value, operation) {}
+	bool compare_tag_types(long long const& value, TagOperation operation, long long const& criteria) const override {
+		switch(operation) {
+			case TagOperation::kEqual:
+				return value == criteria;
+			case TagOperation::kLessThan:
+				return value < criteria ;
+			case TagOperation::kLessThanOrEqual:
+				return value <= criteria;
+			case TagOperation::kGreaterThan:
+				return value > criteria;
+			case TagOperation::kGreaterThanOrEqual:
+				return value >= criteria;
+			default:
+				return false;
+		}
+	}
+};
+
+template<typename T>
+class DoubleTagType : public TagType<T, double> {
+protected:
+	DoubleTagType(double const& value, TagOperation operation) : TagType<T, double>(value, operation) {}
+	bool compare_tag_types(double const& value, TagOperation operation, double const& criteria) const override {
+		switch(operation) {
+			case TagOperation::kEqual:
+				return value == criteria;
+			case TagOperation::kLessThan:
+				return value < criteria ;
+			case TagOperation::kLessThanOrEqual:
+				return value <= criteria;
+			case TagOperation::kGreaterThan:
+				return value > criteria;
+			case TagOperation::kGreaterThanOrEqual:
+				return value >= criteria;
+			default:
+				return false;
+		}
+	}
+};
+
+template<typename T>
+class BoolTagType : public TagType<T, bool> {
+protected:
+	BoolTagType(bool const& value, TagOperation operation) : TagType<T, bool>(value, operation) {}
+	bool compare_tag_types(bool const& value, TagOperation operation, bool const& criteria) const override {
+		switch(operation) {
+			case TagOperation::kEqual:
+				return value == criteria;
+			default:
+				return false;
+		}
+	}
+};
+
+class LogLevel : public StringTagType<LogLevel> {
+public:
+	static constexpr char key[] = "log_level";
+	LogLevel(std::string const& value, TagOperation operation = TagOperation::kNone) : StringTagType(value, operation) {}
+};
 
 inline std::map<std::string, std::unique_ptr<Tag>>& get_default_tags() {
 	static std::map<std::string, std::unique_ptr<Tag>> tags;
@@ -52,116 +218,185 @@ inline void add_default_tag(Tag const& tag) {
 	tags[tag.get_key()] = tag.clone();
 }
 
-inline std::fstream log(std::vector<Tag const*> tags = {}) {
-	auto const now = std::chrono::system_clock::now();
-	std::time_t const start_time = std::chrono::system_clock::to_time_t(now);
-	auto const ms = duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
-	std::fstream log_file("cclog.log", std::ios::app);
-	log_file << '\n'
-			<< std::put_time(std::gmtime(&start_time), "%Y-%m-%dT%H:%M:%S.")
-			<< std::setw(3) << std::setfill('0')
-			<< std::to_string(ms.count());
-	for(auto const& default_tag : get_default_tags()) {
-		if(std::find_if(tags.begin(), tags.end(), [&default_tag](auto const& tag) {
-			return default_tag.first == tag->get_key();	
-		}) == tags.end()) {
-			log_file << " " << default_tag.second->get_text();
-		}
-	}
-	for(auto const& tag : tags) {
-		log_file << " " << tag->get_text();
-	}
-	log_file << " ";
+class FilterClause {
+public:
+	std::vector<std::unique_ptr<Tag>> normal_literals;
+	std::vector<std::unique_ptr<Tag>> inverted_literals;
+};
 
-	return log_file;
+inline std::map<int, FilterClause>& get_filter_clauses() {
+	static std::map<int, FilterClause> clauses;
+	return clauses;
 }
 
+inline int create_filter_clause() {
+	static int current_id = 0;
+	++current_id;
+	auto& clauses = get_filter_clauses();
+	clauses[current_id] = FilterClause();
+	return current_id;
+}
+
+inline void add_filter_literal(int filter_id, Tag const& tag, bool normal = true) {
+	auto& clauses = get_filter_clauses();
+	if(clauses.contains(filter_id)) {
+		if(normal) {
+			clauses[filter_id].normal_literals.push_back(tag.clone());
+		} else {
+			clauses[filter_id].inverted_literals.push_back(tag.clone());
+		}
+	}
+}
+
+inline void clear_filter_clause(int filter_id) {
+	auto& clauses = get_filter_clauses();
+	clauses.erase(filter_id);
+}
+
+class LogStream : public std::stringstream {
+public:
+	LogStream(std::string const& filename, std::ios_base::openmode mode = ios_base::app) : proceed_(true), fs_(filename, mode) {}
+	LogStream(LogStream const& source) = delete;
+	LogStream(LogStream&& source) : std::stringstream(std::move(source)), proceed_(source.proceed_), fs_(std::move(source.fs_)) {}
+	~LogStream() {
+		if(!proceed_) {
+			return;
+		}
+		fs_ << this->str();
+		fs_ << '\n';
+	}
+
+	LogStream& operator=(LogStream const& rhs) = delete;
+	LogStream& operator=(LogStream&& rhs) = delete;
+	
+	void ignore() {
+		proceed_ = false;
+	}
+private:
+	bool proceed_;
+	std::fstream fs_;
+};
+
+inline LogStream log(std::vector<Tag const*> tags = {}) {
+	auto const time = std::chrono::system_clock::now();
+	std::time_t const start_time = std::chrono::system_clock::to_time_t(time);
+	auto const ms = duration_cast<std::chrono::milliseconds>(time.time_since_epoch()) % 1000;
+
+	LogStream ls("cclog.log");
+	ls << std::put_time(std::gmtime(&start_time), "%Y-%m-%dT%H:%M:%S.")
+		<< std::setw(3) << std::setfill('0') << std::to_string(ms.count());
+	
+	std::map<std::string, Tag const*> active_tags;
+    for (auto const& default_tag: get_default_tags())
+    {
+        active_tags[default_tag.first] = default_tag.second.get();
+    }
+    for (auto const& tag: tags)
+    {
+        active_tags[tag->get_key()] = tag;
+    }
+    for (auto const& active_entry: active_tags)
+    {
+        ls << " " << active_entry.second->get_text();
+    }
+    ls << " ";
+
+	bool proceed = true;
+	for (auto const & clause: get_filter_clauses()) {
+		proceed = false;
+		bool all_literals_match = true;
+		for(auto const& normal : clause.second.normal_literals) {
+			if(!active_tags.contains(normal->get_key())) {
+				all_literals_match = false;
+				break;
+			}
+			if(!active_tags[normal->get_key()]->match(*normal)) {
+				all_literals_match = false;
+				break;
+			}
+		}
+		if(!all_literals_match) {
+			continue;
+		}
+		for(auto const& inverted : clause.second.inverted_literals) {
+			if(active_tags.contains(inverted->get_key())) {
+				if(active_tags[inverted->get_key()]->match(*inverted)) {
+					all_literals_match = false;
+				}
+				break;
+			}
+		}
+		if(all_literals_match) {
+			proceed = true;
+			break;
+		}
+	}
+	if(!proceed) {
+		ls.ignore();
+	}
+	return ls;
+}
 
 inline auto log(Tag const& tag1) {
-	return log({&tag1});
+    return log({&tag1});
 }
 
 inline auto log(Tag const& tag1, Tag const& tag2) {
-	return log({&tag1, &tag2});
+    return log({&tag1, &tag2});
 }
 
-inline auto log(Tag const& tag1, Tag const& tag2, Tag const& tag3) {
+inline auto log(Tag const& tag1, Tag const& tag2, Tag const& tag3){
 	return log({&tag1, &tag2, &tag3});
 }
 
-inline std::fstream& operator<<(std::fstream&& fs, Tag const& tag) {
-	fs << to_string(tag);
-	return fs;
-}
+class Color : public StringTagType<Color> {
+public:
+	static constexpr char key[] = "color";
+	Color(std::string const& value, TagOperation operation = TagOperation::kNone) : StringTagType(value, operation) {}
+};
+
+class Size : public StringTagType<Size> {
+public:
+	static constexpr char key[] = "size";
+	Size(std::string const& value, TagOperation operation = TagOperation::kNone) : StringTagType(value, operation) {}
+};
+
+class Count : public IntegerTagType<Count> {
+public:
+	static constexpr char key[] = "count";
+	Count(int value, TagOperation operation = TagOperation::kNone) : IntegerTagType(value, operation) {}
+};
+
+class Identity : public LongLongTagType<Identity> {
+public:
+	static constexpr char key[] = "id";
+	Identity(long long value, TagOperation operation = TagOperation::kNone) : LongLongTagType(value, operation) {}
+};
+
+class Scale : public DoubleTagType<Scale> {
+public:
+	static constexpr char key[] = "scale";
+	Scale(double value, TagOperation operation = TagOperation::kNone) : DoubleTagType(value, operation) {}
+};
+
+class CacheHit : public BoolTagType<CacheHit> {
+public:
+	static constexpr char key[] = "cache_hit";
+	CacheHit(bool value, TagOperation operation = TagOperation::kNone) : BoolTagType(value, operation) {}
+};
 
 } // namespace cclog
 
-class LogLevel : public cclog::Tag {
-public:
-	LogLevel(std::string const& text) : Tag("log_level", text) {}
-	std::unique_ptr<Tag> clone() const override {
-		return std::unique_ptr<Tag>(new LogLevel(*this));
-	}
-};
-
-class Color : public cclog::Tag {
-public:
-	Color(std::string const& text) : Tag("color", text) {}
-	std::unique_ptr<Tag> clone() const override {
-		return std::unique_ptr<Tag>(new Color(*this));
-	}
-};
-
-class Size : public cclog::Tag {
-public:
-	Size(std::string const& text) : Tag("size", text) {}
-	std::unique_ptr<Tag> clone() const override {
-		return std::unique_ptr<Tag>(new Size(*this));
-	}
-};
-
-class Count : public cclog::Tag {
-public:
-	Count(int value) : Tag("count", value) {}
-	std::unique_ptr<Tag> clone() const override {
-		return std::unique_ptr<Tag>(new Count(*this));
-	}
-};
-
-class Identity : public cclog::Tag {
-public:
-	Identity(long long value) : Tag("id", value) {}
-	std::unique_ptr<Tag> clone() const override {
-		return std::unique_ptr<Tag>(new Identity(*this));
-	}
-};
-
-class Scale : public cclog::Tag {
-public:
-	Scale(double value) : Tag("scale", value) {}
-	std::unique_ptr<Tag> clone() const override {
-		return std::unique_ptr<Tag>(new Scale(*this));
-	}
-};
-
-class CacheHit : public cclog::Tag {
-public:
-	CacheHit(bool value) : Tag("cache_hit", value) {}
-	std::unique_ptr<Tag> clone() const override {
-		return std::unique_ptr<Tag>(new CacheHit(*this));
-	}
-};
-
-inline LogLevel error("error");
-inline LogLevel info("info");
-inline LogLevel debug("debug");
-inline Color red("red");
-inline Color green("green");
-inline Color blue("blue");
-inline Size small("small");
-inline Size medium("medium");
-inline Size large("large");
-inline CacheHit cache_hit(true);
-inline CacheHit cache_miss(false);
+inline cclog::LogLevel error("error");
+inline cclog::LogLevel info("info");
+inline cclog::LogLevel debug("debug");
+inline cclog::Color red("red");
+inline cclog::Color green("green");
+inline cclog::Color blue("blue");
+inline cclog::Size small("small");
+inline cclog::Size medium("medium");
+inline cclog::Size large("large");
+inline cclog::CacheHit cache_hit(true);
+inline cclog::CacheHit cache_miss(false);
 
 #endif // CCLOG_H
--- tests/tags.cc
+++ tests/tags.cc
@@ -2,10 +2,26 @@
 #include "utility.h"
 #include "ccunit.h"
 
+class TempFilterClause {
+public:
+	void setup() {
+		id_ = cclog::create_filter_clause();
+	}
+	void teardown() {
+		cclog::clear_filter_clause(id_);
+	}
+	int get_id() const {
+		return id_;
+	}
+private:
+	int id_;
+};
+
 TEST("Message can be tagged in log") {
 	std::string message = "simple tag ";
 	message += Utility::random_string();
 	cclog::log(error) << message;
+
 	std::string log_level_tag = " log_level=\"error\" ";
 	std::string default_log_level_tag = " log_level=\"info\" ";
 	bool result = Utility::is_text_in_file(message, "cclog.log", {log_level_tag}, {default_log_level_tag});
@@ -30,6 +46,7 @@ TEST("Default tags set in main appear in log") {
 TEST("Multiple tags can be used in log") {
 	std::string message = "multi tags ";
 	message += Utility::random_string();
+
 	cclog::log(debug, red, large) << message;
 
 	std::string log_level_tag = " log_level=\"debug\" ";
@@ -42,7 +59,7 @@ TEST("Multiple tags can be used in log") {
 TEST("Tags can be streamed to log") {
 	std::string message_base = " 1 type ";
 	std::string message = message_base + Utility::random_string();
-	cclog::log(info) << Count(1) << message;
+	cclog::log(info) << cclog::Count(1) << message;
 
 	std::string count_tag = " count=1 ";
 	bool result = Utility::is_text_in_file(message, "cclog.log", {count_tag});
@@ -50,14 +67,16 @@ TEST("Tags can be streamed to log") {
 
 	message_base = " 2 type ";
 	message = message_base + Utility::random_string();
-	cclog::log(info) << Identity(123456789012345) << message;
+	cclog::log(info) << cclog::Identity(123456789012345) << message;
+
 	std::string id_tag = " id=123456789012345 ";
 	result = Utility::is_text_in_file(message, "cclog.log", {id_tag});
 	CONFIRM_TRUE(result);
 
 	message_base = " 3 type ";
 	message = message_base + Utility::random_string();
-	cclog::log(info) << Scale(1.5) << message;
+	cclog::log(info) << cclog::Scale(1.5) << message;
+
 	std::string scale_tag = " scale=1.500000 ";
 	result = Utility::is_text_in_file(message, "cclog.log", {scale_tag});
 	CONFIRM_TRUE(result);
@@ -65,7 +84,72 @@ TEST("Tags can be streamed to log") {
 	message_base = " 4 type ";
 	message = message_base + Utility::random_string();
 	cclog::log(info) << cache_miss << message;
+
 	std::string cache_tag = " cache_hit=false ";
 	result = Utility::is_text_in_file(message, "cclog.log", {cache_tag});
+	CONFIRM_TRUE(result);
+}
+
+TEST("Tags can be used to filter message") {
+	ccunit::SetupAndTeardown<TempFilterClause> filter;
+	cclog::add_filter_literal(filter.get_id(), error);
+
+	std::string message = "filter ";
+	message += Utility::random_string();
+	cclog::log(info) << message;
+	bool result = Utility::is_text_in_file(message, "cclog.log");
+	CONFIRM_FALSE(result);
+
+	cclog::clear_filter_clause(filter.get_id());
+	cclog::log(info) << message;
+	result = Utility::is_text_in_file(message, "cclog.log");
+	CONFIRM_TRUE(result);
+}
+
+TEST("Overridden default tag not used to filter messages") {
+	ccunit::SetupAndTeardown<TempFilterClause> filter;
+	cclog::add_filter_literal(filter.get_id(), info);
+
+	std::string message = "override default ";
+	message += Utility::random_string();
+	cclog::log(debug) << message;
+
+	bool result = Utility::is_text_in_file(message, "cclog.log");
+	CONFIRM_FALSE(result);
+}
+
+TEST("Inverted tag can be used to filter messages") {
+	ccunit::SetupAndTeardown<TempFilterClause> filter;
+	cclog::add_filter_literal(filter.get_id(), green, false);
+
+	std::string message = "inverted ";
+	message += Utility::random_string();
+	cclog::log(info) << message;
+
+	bool result = Utility::is_text_in_file(message, "cclog.log");
+	CONFIRM_FALSE(result);
+}
+
+TEST("Tag values can be used to filter messages") {
+	ccunit::SetupAndTeardown<TempFilterClause> filter;
+	cclog::add_filter_literal(filter.get_id(), cclog::Count(100, cclog::TagOperation::kGreaterThan));
+
+	std::string message = "values ";
+	message += Utility::random_string();
+
+	cclog::log(cclog::Count(1)) << message;
+
+	bool result = Utility::is_text_in_file(message, "cclog.log");
+	CONFIRM_FALSE(result);
+
+	cclog::log() << cclog::Count(101) << message;
+
+	result = Utility::is_text_in_file(message, "cclog.log");
+	CONFIRM_FALSE(result);
+
+
+	cclog::log(cclog::Count(101)) << message;
+
+	result = Utility::is_text_in_file(message, "cclog.log");
 	CONFIRM_TRUE(result);
 }
